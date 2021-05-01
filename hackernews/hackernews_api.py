8201d8bc62493475c0429f76f739b632dcc0ab65
Namanh Tran <NamanhDTran@gmail.com>
2021-05-01 00:00:33 -0700

    added exceptions for failed requests

--- hackernews/hackernews_api.py
+++ hackernews/hackernews_api.py
@@ -1,18 +1,19 @@
 import requests, json
 
 def get_hackernews_stories(max_stories):
+    try:
+        response = requests.get("https://hacker-news.firebaseio.com/v0/topstories.json")
 
-    response = requests.get("https://hacker-news.firebaseio.com/v0/topstories.json")
+        stories_id = response.json()[:max_stories]
 
-    stories_id = response.json()[:max_stories]
+        stories_info = []
 
-    stories_info = []
-
-    for story_id in stories_id:
-        response = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json")
-        story_info = response.json()
-        stories_info.append(story_info)
-
-    return stories_info
-
-print(get_hackernews_stories(20))
+        for story_id in stories_id:
+            response = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json")
+            story_info = response.json()
+            stories_info.append(story_info)
+            
+        return stories_info
+    
+    except:
+        return None
