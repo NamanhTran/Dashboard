92ace7f9780552626209a2f3f3191b22a1711cf9
Namanh Tran <NamanhDTran@gmail.com>
2021-05-01 00:02:09 -0700

    added exceptions and removed getting api key from config

--- nytimes/nytimes_api.py
+++ nytimes/nytimes_api.py
@@ -1,14 +1,17 @@
 import requests, json, configparser
 
-def get_nytimes_articles(max_articles):
+def get_nytimes_articles(api_key, max_articles):
     # Read from the config file for nytime api key
-    config = configparser.ConfigParser()
-    config.read('../config.cfg')
+    # config = configparser.ConfigParser()
+    # config.read('../configs/config.cfg')
     
     # Get the articles from the front page
-    response = requests.get(f"https://api.nytimes.com/svc/topstories/v2/home.json?api-key={config['API Key']['nytimes']}")
+    try:
+        response = requests.get(f"https://api.nytimes.com/svc/topstories/v2/home.json?api-key={api_key}")
 
-    # Only get the first n articles
-    articles_json = response.json()['results'][:max_articles]
-    print(json.dumps(articles_json, indent=4))
-    return articles_json
+        # Only get the first n articles
+        articles_json = response.json()['results'][:max_articles]
+        return articles_json
+
+    except:
+        return None
